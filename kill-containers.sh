fc387ff83288b38419fa9b77845d7a29a532b06c
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-09-20 09:47:13 -0300

    try docker kill without double quotes to introduce word splitting

--- kill-containers.sh
+++ kill-containers.sh
@@ -1,7 +1,7 @@
 docker ps -q | sort > /tmp/post-containers-list.txt
 containers_to_kill=$(comm -23 /tmp/post-containers-list.txt /tmp/pre-containers-list.txt)
 
-if ! docker kill "$containers_to_kill" 2>&1 >/dev/null; then
+if ! docker kill $containers_to_kill 2>&1 >/dev/null; then
   echo nothing to clean
 else
   echo cleaning up after killing containers
